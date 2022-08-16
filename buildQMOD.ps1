2057caae971ea033d7e8f42c535585980ce99f41
keganpowers34 <42286354+keganpowers34@users.noreply.github.com>
2022-08-16 13:00:19 -0600

    Update buildQMOD.ps1

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -1,22 +1,12 @@
 Param(
-    [Parameter(Mandatory=$false, HelpMessage="The name the output qmod file should have")][String] $qmodname="IntroSkip",
+    [String] $qmodname="IntroSkip",
 
-    [Parameter(Mandatory=$false, HelpMessage="Switch to create a clean compilation")]
-    [Alias("rebuild")]
+    [Parameter(Mandatory=$false)]
     [Switch] $clean,
 
-    [Parameter(Mandatory=$false, HelpMessage="Prints the help instructions")]
-    [Switch] $help,
-
-    [Parameter(Mandatory=$false, HelpMessage="Tells the script to not compile and only package the existing files")]
-    [Alias("actions", "pack")]
-    [Switch] $package,
-
-    [Parameter(Mandatory=$false, HelpMessage="The version of the mod")][String] $version
+    [Parameter(Mandatory=$false)]
+    [Switch] $help
 )
-
-# Builds a .qmod file for loading with QP or BMBF
-
 
 if ($help -eq $true) {
     echo "`"BuildQmod <qmodName>`" - Copiles your mod into a `".so`" or a `".a`" library"
@@ -25,9 +15,7 @@ if ($help -eq $true) {
 
     echo "`n-- Arguments --`n"
 
-    echo "-clean `t`t Performs a clean build on both your library and the qmod"
-    echo "-help `t`t Prints this"
-    echo "-package `t Only packages existing files, without recompiling`n"
+    echo "-Clean `t`t Performs a clean build on both your library and the qmod"
 
     exit
 }
@@ -38,24 +26,11 @@ if ($qmodName -eq "")
     exit
 }
 
-if ($package -eq $true) {
-    $qmodName = "$($env:module_id)_$($env:version)"
-    echo "Actions: Packaging QMod $qmodName"
-}
-if (($args.Count -eq 0) -And $package -eq $false) {
-echo "Creating QMod $qmodName"
-    if ($version.Length -gt 0) {
-        $qmodName += $version
-        qpm-rust package edit --version $version
-    }
-    & $PSScriptRoot/build.ps1 -clean:$clean -version:$version
+& $PSScriptRoot/build.ps1 -clean:$clean
 
-    if ($LASTEXITCODE -ne 0) {
-        echo "Failed to build, exiting..."
-        exit $LASTEXITCODE
-    }
-
-    qpm-rust qmod build
+if ($LASTEXITCODE -ne 0) {
+    echo "Failed to build, exiting..."
+    exit $LASTEXITCODE
 }
 
 echo "Creating qmod from mod.json"
@@ -69,8 +44,6 @@ $cover = "./" + $modJson.coverImage
 if ((-not ($cover -eq "./")) -and (Test-Path $cover))
 { 
     $filelist += ,$cover
-} else {
-    echo "No cover Image found"
 }
 
 foreach ($mod in $modJson.modFiles)
@@ -85,10 +58,10 @@ foreach ($mod in $modJson.modFiles)
 
 foreach ($lib in $modJson.libraryFiles)
 {
-    $path = "./extern/libs/" + $lib
+    $path = "./build/" + $lib
     if (-not (Test-Path $path))
     {
-        $path = "./build/" + $lib
+        $path = "./extern/libs/" + $lib
     }
     $filelist += $path
 }
@@ -104,5 +77,3 @@ if ((-not ($clean.IsPresent)) -and (Test-Path $qmod))
 
 Compress-Archive -Path $filelist -DestinationPath $zip -Update
 Move-Item $zip $qmod -Force
-
-echo "Task Completed"
